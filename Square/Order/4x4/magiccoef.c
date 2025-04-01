038b1111784a9e047594d519534b75134d037096
Gavin <anthonyforrester@proton.me>
2025-04-01 17:36:12 +0000

    Add files via upload

--- Square/Order/4x4/magiccoef.c
+++ Square/Order/4x4/magiccoef.c
@@ -1,43 +1,38 @@
 // REFINED CODE
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define MAX 10000
+#define MAX 1000
 #define LIM 880*16
 
-int tally, val[MAX], qty[MAX]; char dir[MAX]; 
+int tally, id, *val, *qty, *out;
 int dat[LIM];
 
-void calcoef(int ort, int a, int b, int c, int d){
+void calcoef(int a, int b, int c, int d){
 	int cal=(d*c)+(d*b)+(c*b)+(d*a)+(c*a)+(a*b);
 	val[cal]=cal; qty[cal]++;
-	switch(ort){
-		case 1:
-			dir[cal]='r'; //Row
-			break;
-		case 2:
-			dir[cal]='c'; //Column
-			break;
-		case 3:
-			dir[cal]='d'; //Diagonal
-			break;
-		default:
-			dir[cal]='~'; //Invaild
-	}
 }
 
 void searchcoef(int *s){
 	for(int i=0; i<16; i+=4){
-		calcoef(1, s[0+i], s[1+i], s[2+i], s[3+i]);
+		calcoef(s[0+i], s[1+i], s[2+i], s[3+i]);
 	} for(int j=0; j<4; j++){
-		calcoef(2, s[0+j], s[4+j], s[8+j], s[12+j]);}
-	calcoef(3, s[0], s[5], s[10], s[15]);
-	calcoef(3, s[3], s[6], s[9], s[12]);
+		calcoef(s[0+j], s[4+j], s[8+j], s[12+j]);}
+	calcoef(s[0], s[5], s[10], s[15]); calcoef(s[3], s[6], s[9], s[12]);
 }
 
-void readout(int *m){
+
+void reset(int *m){for(int i=0; i<MAX; i++){m[i]=0;}}
+
+void readout(){
 	for(int i=0; i<MAX; i++){
-		if(qty[i]<2){val[i]=0;};
-		if(val[i]!=0){tally++; printf("tally: %d | quantity: %d | direction: %c | coefficient: %d\n", tally, qty[i], dir[i], val[i]);}}
+		if(qty[i]<2){val[i]=0; out[val[i]]=0;};
+		if(val[i]!=0){printf("[%d, %d, %d]\n", id, qty[i], val[i]); out[val[i]]=val[i];}};
+}
+
+void complete(){
+	printf("\n"); for(int j=0; j<MAX; j++){if(out[j]!=0){tally++; printf("[%d, %d]\n", tally, out[j]);}}
 }
 
 void feed(char file[]){
@@ -53,10 +48,17 @@ void feed(char file[]){
 void filereadin(){
 	int step=16;
 	int matrix[step];
+	val=(int*)malloc(MAX*sizeof(int));
+	qty=(int*)malloc(MAX*sizeof(int));
+	out=(int*)malloc(MAX*sizeof(int));
 	for(int i=0; i<LIM; i+=step){for(int j=0; j<step; j++){
 			matrix[j]=dat[i+j];
-		}; searchcoef(matrix);
-  }
+		}; searchcoef(matrix); readout(); id++;
+			reset(val);
+			reset(qty);
+			printf("\n");
+  	} complete(); free(val); free(qty);
+	free(out);
 }
 
 int main(){
@@ -66,6 +68,6 @@ int main(){
 		7, 9, 12, 6,
 		14, 4, 1, 15}; //A vaild magic square
 	searchcoef(magic); readout(val);*/
-	feed("squares.txt"); filereadin(); readout(val);
+	feed("squares.txt"); filereadin();
 	return 0;
 }
